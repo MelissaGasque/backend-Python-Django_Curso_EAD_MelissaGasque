7fec1994fbb785f399b8ef3d71c711a15784e444
Melissa Gasque <melissagasque.com>
2024-01-11 15:10:49 -0300

    Adicionado DER ((Diagrama de Entidade e Relacionamento) da aplicação

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path("accounts/", AccountView.as_view()),
-    path("login/", jwt_views.TokenObtainPairView.as_view())
+    path("login/", jwt_views.TokenObtainPairView.as_view()),
+    path("token/refresh/", jwt_views.TokenRefreshView.as_view())
 ]
